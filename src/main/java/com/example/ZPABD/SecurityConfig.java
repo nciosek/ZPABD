2ff35dfbb7361d0b685448765a28dc5dbd1098bb
Natalia Ciosek <n_ciosek@wp.pl>
2020-12-15 17:52:56 +0100

    add security config

--- src/main/java/com/example/ZPABD/SecurityConfig.java
+++ src/main/java/com/example/ZPABD/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.ZPABD;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
